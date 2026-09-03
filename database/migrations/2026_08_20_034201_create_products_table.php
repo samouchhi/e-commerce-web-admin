760e69721202fafe74a1ab556fa2d7cf9da1c7ea
Ouchhi <samouchhi@gmail.com>
2026-09-03 14:47:19 +0700

    fix: remove unnecessary migration files and clean up product and purchase tables

--- database/migrations/2026_08_20_034201_create_products_table.php
+++ database/migrations/2026_08_20_034201_create_products_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
