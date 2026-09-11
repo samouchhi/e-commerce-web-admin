2bb239418312a711e7f9ff24c73f006720d1f83e
Ouchhi <samouchhi@gmail.com>
2026-09-11 16:34:47 +0700

    chore: change navbar to top

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('dashboard')
             ->path('dashboard')
-            ->topbar(false)
+            ->topbar(true)
             ->databaseNotifications()
             ->viteTheme('resources/css/filament/dashboard/theme.css')
 
